e685094be90f2f4cc41daa25af60c802436b5516
Nguyen Cong Hieu <hieucong2468@gmail.com>
2025-02-12 23:20:28 +0700

    add logo to titlte and change in main option

--- hehe.js
+++ hehe.js
@@ -13,3 +13,25 @@ function handleClickAccordion(event){
     icon.classList.toggle("fa-angle-down");
     icon.classList.toggle("fa-angle-up");
 }
+
+
+function updateValue(inputId, spanId) {
+    document.getElementById(spanId).textContent = document.getElementById(inputId).value;
+}
+
+// Cập nhật giá trị ban đầu của thanh trượt
+document.addEventListener("DOMContentLoaded", function () {
+    let sliders = ["form-width", "form-height", "form-margin"];
+    sliders.forEach(id => {
+        let input = document.getElementById(id);
+        let span = document.getElementById(id.replace("form-", "") + "-value"); // Tạo ID tương ứng
+        span.textContent = input.value; // Hiển thị giá trị ban đầu
+
+        // Lắng nghe sự kiện thay đổi giá trị
+        input.addEventListener("input", function () {
+            span.textContent = input.value;
+        });
+    });
+});
+
+
