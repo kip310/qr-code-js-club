3969feb319e0f44d3e7545b39236a12f0f1a730c
Haipm26 <haiphamminh2k6@gmail.com>
2025-03-03 01:14:14 +0700

    done

--- userAvatar.js
+++
@@ -1,6 +0,0 @@
-if (user) {
-    const avatarImg = document.querySelector(".avatar-img");
-    if (avatarImg && user.user_metadata?.avatar_url) {
-      avatarImg.src = user.user_metadata.avatar_url;
-    }
-  }
