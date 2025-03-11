f593b64f31b9ec7cd1c60cc5c10aab8c920a5663
unknown <duckip310@gmail.com>
2025-03-11 18:51:33 +0700

    fe contactus

--- contact/contactus.js
+++ contact/contactus.js
@@ -16,3 +16,29 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init({ publicKey: "kPy0S-rBy3x5omKHQ" });
+  // Thay YOUR_USER_ID bằng User ID của bạn
+
+  document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Ngăn chặn tải lại trang khi gửi biểu mẫu
+
+    let formData = {
+      name: document.querySelector('input[name="name"]').value,
+      email: document.querySelector('input[name="email"]').value,
+      phone: document.querySelector('input[name="phone"]').value,
+      message: document.querySelector('textarea[name="message"]').value,
+    };
+
+    emailjs
+      .send("service_zf2ej2t", "template_e3iurgn", formData) // Thay bằng Service ID và Template ID
+      .then(
+        function (response) {
+          alert("Gửi tin nhắn thành công!");
+        },
+        function (error) {
+          alert("Gửi tin nhắn thất bại: " + JSON.stringify(error));
+        }
+      );
+  });
+});
